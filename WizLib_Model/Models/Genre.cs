d0d577dec2368144ed688c537884c38a3d6f1a3d
h.sadeghi <hossein.sadeghikia@gmail.com>
2021-03-19 13:27:45 +0330

    End of Section 3

--- WizLib_Model/Models/Genre.cs
+++ WizLib_Model/Models/Genre.cs
@@ -1,9 +1,14 @@
-﻿namespace WizLib_Model.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WizLib_Model.Models
 {
+    [Table("tb_Genre")]
     public class Genre
     {
         public int GenreId { get; set; }
+
+        [Column("Name")]
         public string GenreName { get; set; }
-        public int DisplayOrder { get; set; }
+        //public int DisplayOrder { get; set; }
     }
 }
